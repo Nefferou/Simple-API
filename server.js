cb4f3843578a46a65ba9a372cd63b18b711fc73e
Nefferou <fertijulien@gmail.com>
2025-06-12 14:08:30 +0200

    Modif ci cd and server.js

--- server.js
+++ server.js
@@ -11,18 +11,6 @@ app.get('/status', (req, res) => {
     res.json({ status: 'OK' });
 });
 
-/*
-// Démarrage local
-if (require.main === module) {
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-        console.log(`Server running locally on port ${port}`);
-    });
-}
-
-const serverless = require('serverless-http');
-module.exports = serverless(app);*/
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running locally on port ${port}`);
